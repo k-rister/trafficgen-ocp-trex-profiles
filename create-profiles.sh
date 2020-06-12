a8eb78053173f6b890e11c0f6f371e088b1553c9
Karl Rister <krister@redhat.com>
2020-06-11 19:10:23 -0500

    make the TRex MAC addresses configurable

--- create-profiles.sh
+++ create-profiles.sh
@@ -2,11 +2,13 @@
 # vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 filetype=bash
 # -*- mode: sh; indent-tabs-mode: nil; sh-basic-offset: 4 -*-
 
-MAC_A=${1}
-MAC_B=${2}
+TREX_MAC_A=${1}
+TREX_MAC_B=${2}
+DUT_MAC_A=${3}
+DUT_MAC_B=${4}
 
-if [ -z "${MAC_A}" -o -z "${MAC_B}" ]; then
-    echo "ERROR: You must specify both MAC_A and MAC_B as ordered arguments"
+if [ -z "${TREX_MAC_A}" -o -z "${TREX_MAC_B}" -o -z "${DUT_MAC_A}" -o -z "${DUT_MAC_B}" ]; then
+    echo "ERROR: You must specify TREX_MAC_A, TREX_MAC_B, DUT_MAC_A, and DUT_MAC_B as ordered arguments"
     exit 1
 fi
 
@@ -16,7 +18,7 @@ if pushd ${CWD}/base > /dev/null; then
     mkdir -p ../configured
 
     for profile in $(ls -1 *.json); do
-	cat ${profile} | sed -e "s/MAC_A/${MAC_A}/" -e "s/MAC_B/${MAC_B}/" > ../configured/${profile}
+	cat ${profile} | sed -e "s/TREX_MAC_A/${TREX_MAC_A}/" -e "s/TREX_MAC_B/${TREX_MAC_B}/" -e "s/DUT_MAC_A/${DUT_MAC_A}/" -e "s/DUT_MAC_B/${DUT_MAC_B}/" > ../configured/${profile}
     done
 
     ls -l ../configured
